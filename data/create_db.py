acc57d1bbc2f593ff8c814e0a600dc1eee556fc1
timre13 <itorteli13@gmail.com>
2024-01-11 13:19:36 +0100

    Add "sortav", "totav"

--- data/create_db.py
+++ data/create_db.py
@@ -1,14 +1,24 @@
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("neighbours.xml")
+
 root = tree.getroot()
+
+class Plant:
+    def __init__(self, name, sortav, totav):
+        self.name = name
+        self.sortav = sortav
+        self.totav = totav
 
 plants = set()
 goodPairs = set()
 badPairs = set()
 
 for child in root:
-    plants.add(child.attrib["name"])
+    plants.add(Plant(
+        child.attrib["name"],
+        child.attrib.get("sortav", 0),
+        child.attrib.get("totav", 0)))
     for child1 in child:
         if child1.tag == "good":
             goodPairs.add(".".join(sorted([child.attrib["name"], child1.text])))
@@ -24,12 +34,17 @@ cur.execute("DROP TABLE IF EXISTS plants")
 cur.execute("""
     CREATE TABLE plants (
         id INTEGER UNIQUE NOT NULL PRIMARY KEY,
-        name TEXT UNIQUE NOT NULL
+        name TEXT UNIQUE NOT NULL,
+        sortavolsag INTEGER NOT NULL,
+        totavolsag INTEGER NOT NULL,
+        color STRING
     )
 """)
 
 for plant in tuple(plants):
-    cur.execute("INSERT INTO plants (name) VALUES (?)", [plant])
+    cur.execute("""
+    INSERT INTO plants (name, sortavolsag, totavolsag, color)
+    VALUES (?, ?, ?, ?)""", [plant.name, plant.sortav, plant.totav, "#33dd55"])
 
 cur.execute("DROP TABLE IF EXISTS good_neighbours")
 cur.execute("""
