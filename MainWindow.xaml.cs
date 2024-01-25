d84146f25e45056fd25b2cd2879ad80738c8d33b
0324bence <d.bence0324@gmail.com>
2024-01-25 12:40:14 +0100

    Add context menu to plant list

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,31 +26,81 @@ namespace garden_planner
             InitializeComponent();
         }
 
-        public struct PlantListItem
-        {
-            public Plant plant;
-            public bool bad;
-            public bool good;
-        }
-
         private void Root_Loaded(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("Root_Loaded");
             Root.Items.Clear();
             var plants = Database.GetAllPlantsOrdered();
+            
             foreach (var plant in plants)
             {
                 if (plant == null) continue;
+                int amount = 0;
+                if (plantAmounts.ContainsKey(plant.Id))
+                {
+                    amount = plantAmounts[plant.Id];
+                }
+                
                 var item = new
                 {
                     plant,
                     bad = false,
-                    good = false
+                    good = false,
+                    amount
                 };
                 Root.Items.Add(item);
             }
         }
 
         private void Root_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var currentItemI = Root.SelectedIndex;
+            dynamic currentItem = Root.SelectedItem;
+            if (currentItem?.plant == null) return;
+            MenuUpDown.Value = currentItem.amount;
+            RefreshPlantList();
+        }
+
+        private void AddPlantButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddPlantDialog();
+            dialog.Closed += (s, e) => Root_Loaded(null, null);
+            dialog.ShowDialog();
+        }
+
+        private void MenuDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Dictionary<long, int> plantAmounts = new Dictionary<long, int>();
+
+        private void MenuIntegerUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var currentItemI = Root.SelectedIndex;
+            dynamic currentItem = Root.SelectedItem;
+            if (currentItem?.plant == null) return;
+            Plant selectedPlant = currentItem.plant;
+            if (MenuUpDown.Value == null)
+            {
+                return;
+            }
+            if (plantAmounts.ContainsKey(selectedPlant.Id))
+            {
+                plantAmounts[selectedPlant.Id] = (int)MenuUpDown.Value;
+            }
+            else
+            {
+                plantAmounts.Add(selectedPlant.Id, (int)MenuUpDown.Value);
+            }
+        }
+
+        private void RefreshPlantList()
         {
             var currentItemI = Root.SelectedIndex;
             dynamic currentItem = Root.SelectedItem;
@@ -62,23 +112,34 @@ namespace garden_planner
             {
                 var item = Root.Items[i];
                 var plant = (Plant)((dynamic)item).plant;
+                if (plant == null) continue;
+                int amount = 0;
+                if (plantAmounts.ContainsKey(plant.Id))
+                {
+                    amount = plantAmounts[plant.Id];
+                }
+
                 if (goods.Contains(plant.Id))
                 {
                     Root.Items[i] = new
                     {
                         good = true,
                         bad = false,
-                        plant
+                        plant,
+                        amount
                     };
-                } else if (bads.Contains(plant.Id))
+                }
+                else if (bads.Contains(plant.Id))
                 {
                     Root.Items[i] = new
                     {
                         good = false,
                         bad = true,
-                        plant
+                        plant,
+                        amount
                     };
-                } else
+                }
+                else
                 {
                     if (((dynamic)item).good || ((dynamic)item).bad)
                     {
@@ -86,7 +147,8 @@ namespace garden_planner
                         {
                             good = false,
                             bad = false,
-                            plant
+                            plant,
+                            amount
                         };
                     }
                 }
@@ -94,10 +156,9 @@ namespace garden_planner
             Root.SelectedIndex = currentItemI;
         }
 
-        private void AddPlantButton_Click(object sender, RoutedEventArgs e)
+        private void ContextMenu_Closed(object sender, RoutedEventArgs e)
         {
-            var dialog = new AddPlantDialog();
-            dialog.ShowDialog();
+            Root_Loaded(null, null);
         }
     }
 }
