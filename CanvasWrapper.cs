4691431e25e48596d023c6fa9a873f939de889e3
timre13 <itorteli13@gmail.com>
2024-02-14 20:24:53 +0100

    Mark plants with a small circle

--- CanvasWrapper.cs
+++ CanvasWrapper.cs
@@ -64,6 +64,7 @@ namespace garden_planner
 
         public void DrawPlant(in Plant plant, int x, int y)
         {
+#if false
             var c1 = ((Color)ColorConverter.ConvertFromString(plant.Color));
             c1.A = 100;
             var shape1 = new Rectangle()
@@ -78,17 +79,30 @@ namespace garden_planner
             Canvas.SetTop(shape1, (y - plant.Sortavv));
             canvas.Children.Add(shape1);
 
-            // var shape = new Ellipse
-            // {
-            //     Width = plant.Sortav * 2 ?? 10,
-            //     Height = plant.Sortav * 2?? 10,
-            //     Fill = new SolidColorBrush(c1),
-            //     Stroke = new SolidColorBrush(Colors.Black),
-            //     StrokeThickness = 1
-            // };
-            // Canvas.SetLeft(shape, (double)(x - plant.Sortav!));
-            // Canvas.SetTop(shape, (double)(y - plant.Sortav!));
-            // canvas.Children.Add(shape);
+            var shape = new Ellipse
+            {
+                Width = plant.Sortav * 2 ?? 10,
+                Height = plant.Sortav * 2?? 10,
+                Fill = new SolidColorBrush(c1),
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1
+            };
+            Canvas.SetLeft(shape, (double)(x - plant.Sortav!));
+            Canvas.SetTop(shape, (double)(y - plant.Sortav!));
+            canvas.Children.Add(shape);
+#else
+            var shape = new Ellipse
+            {
+                Width = 20,
+                Height = 20,
+                Fill = new SolidColorBrush(((Color)ColorConverter.ConvertFromString(plant.Color))),
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1
+            };
+            Canvas.SetLeft(shape, x - 10);
+            Canvas.SetTop(shape, y - 10);
+            canvas.Children.Add(shape);
+#endif
 
             var line1 = new Line()
             {
